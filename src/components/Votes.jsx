df0477a852356d72b6f4eb3a5165aa059f48cd50
pippaamy <pippa_austin@live.com>
2022-08-24 11:57:16 +0100

    voting with catch block and show your one vote

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -1,20 +1,46 @@
 import { useState } from "react";
+import { patchArticle } from "../api";
 
 const Votes = ({ article }) => {
-  const [votes, setVotes] = useState(article.votes);
+  const [votes, setVotes] = useState(0);
 
-  const handleClick = () => {
-    setVotes(article.votes + 1);
+  const upVote = () => {
+    setVotes((currentVotes) => {
+      return currentVotes + 1;
+    });
+    patchArticle(article.article_id, 1).catch(() => {
+      setVotes((currentVotes) => {
+        return currentVotes - 1;
+      });
+    });
   };
-  console.log(votes);
+  const downVote = () => {
+    setVotes((currentVotes) => {
+      return currentVotes - 1;
+    });
+    patchArticle(article.article_id, -1).catch(() => {
+      setVotes((currentVotes) => {
+        return currentVotes + 1;
+      });
+    });
+  };
 
   return (
     <div>
-      <p>Votes :{article.votes}</p>
-      <button onClick={handleClick} className="upVote">
-        Give me a vote!
+      <p>Votes :{article.votes + votes}</p>
+      <button
+        onClick={() => upVote(article.article_id, true)}
+        className="upVote"
+      >
+        Vote up!
       </button>
-      <button className="downVote"> I don't like this </button>
+      <button
+        onClick={() => downVote(article.article_id, true)}
+        className="downVote"
+      >
+        {" "}
+        Vote down{" "}
+      </button>
     </div>
   );
 };
